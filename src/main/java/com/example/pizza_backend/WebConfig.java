472c1077c91c1793e5bcf1151180767a23e9912c
lookchub <Ceekay662005@gmail.com>
2025-10-17 15:01:30 +0700

    add cors

--- src/main/java/com/example/pizza_backend/WebConfig.java
+++ src/main/java/com/example/pizza_backend/WebConfig.java
@@ -41,7 +41,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000","http://localhost:8888")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
